c603f65bcdb689f5fec5e6ad5426147a963ddd22
Victor Jimenez <vjimenez@vistaprint.com>
2017-05-05 19:34:43 +0200

    Added sample files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Microsoft.Extensions.Configuration;
+
+namespace aws_table_prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var options = config.GetAWSOptions();
+            var client = options.CreateServiceClient<IAmazonDynamoDB>();
+            var contextConfig = new DynamoDBContextConfig();
+
+            Console.WriteLine($"prefix:{contextConfig.TableNamePrefix}");
+        }
+    }
+}
